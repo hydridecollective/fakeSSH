8370e6bb3159904247450ebfdf72bc875ac81806
Dani <git@danny.works>
2023-08-19 15:34:12 -0700

    forced to use manual daemonisation

--- resources/start.sh
+++ resources/start.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-sh -c "rc-status; rc-service sshd start"
-
-# keep container running
-tail -f /dev/null
-
-# trap ctrl-c and kill sshd process
-trap "rc-service sshd stop" SIGINT
-
-# trap kill sshd process
-trap "rc-service sshd stop" SIGTERM
+sh -c "/usr/bin/sshd -De"
